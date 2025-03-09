9fd2d338c2224c4e944a6e795950b8a1c312488d
thanhanh1182003 <66540569+thanhanh1182003@users.noreply.github.com>
2025-03-09 10:17:36 +0700

    lam lai

--- nguyen_thanh_anh/main.c
+++ nguyen_thanh_anh/main.c
@@ -4,11 +4,53 @@ input:
       int a: so nguyen thu nhat
       int b: so nguyen thu hai
 output:
-      int x: ket qua phep tinh
+      int: ket qua phep tinh
 */
 int tru(int a, int b)
 {
     int ketqua = 0;
-    ketqua = a + b;
+    ketqua = a - b;
+    return ketqua;
+}
+/*
+chuc nang: nhan 2 so nguyen
+input:
+      int a: so nguyen thu nhat
+      int b: so nguyen thu hai
+output:
+      int: ket qua phep tinh
+*/
+int nhan(int a, int b)
+{
+    int ketqua = 0;
+    ketqua = a * b;
+    return ketqua;
+}
+/*
+chuc nang: chia 2 so nguyen
+input:
+      int a: so nguyen thu nhat
+      int b: so nguyen thu hai
+output:
+      int: ket qua phep tinh
+*/
+int chia(int a, int b)
+{
+    int ketqua = 0;
+    ketqua = a * b;
+    return ketqua;
+}
+/*
+chuc nang: cong 2 so nguyen
+input:
+      int a: so nguyen thu nhat
+      int b: so nguyen thu hai
+output:
+      int: ket qua phep tinh
+*/
+int cong(int a, int b)
+{
+    int ketqua = 0;
+    ketqua = a * b;
     return ketqua;
 }
