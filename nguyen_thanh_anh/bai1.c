65b9f2198d95cdbf4d0bf9f1d5e63f6d959abcc5
thanhanh1182003 <66540569+thanhanh1182003@users.noreply.github.com>
2025-03-18 23:11:04 +0700

    đếm khý tự trong chuỗi

--- nguyen_thanh_anh/bai1.c
+++ nguyen_thanh_anh/bai1.c
@@ -1,13 +1,14 @@
 ﻿#include <stdio.h>
+
 /*xay dung  ham dem so ky tu trong chuoi */
 static int demkytu(char chuoi[])
 {
-	int i =0;
-	while (chuoi[i] != '\0') /*đếm đến khi bằng kí tự 0*/
+	int dem=0;
+	while (int i,chuoi[i] != '\0') /*đếm đến khi bằng kí tự 0*/
 	{
-		 i++;
+		 dem++;
 	}
-	return i ;
+	return dem;
 }
 void main()
 {
