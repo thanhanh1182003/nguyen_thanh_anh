6f6e3f1f98504e0678eda49573248e49038f3b12
thanhanh1182003 <66540569+thanhanh1182003@users.noreply.github.com>
2025-03-18 22:48:21 +0700

    bai1

---
+++ nguyen_thanh_anh/bai1.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+/*xay dung  ham dem so ky tu trong chuoi */
+static int demkytu(char chuoi[])
+{
+	int i =0;
+	while (chuoi[i] != '\0') /*đếm đến khi bằng kí tự 0*/
+	{
+		 i++;
+	}
+	return i ;
+}
+void main()
+{
+	char chuoi[] = "nguyen thanh anh";
+	int sokytu = demkytu(chuoi);
+	printf("so ky tu chuoi la: %d", sokytu);
+}
